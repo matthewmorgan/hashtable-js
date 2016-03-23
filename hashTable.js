9449e9c6ce23e7c265d471f3b20d0086e2bd045d
Matthew Morgan <matt@dulcetsoftware.com>
2016-03-23 09:04:38 -0400

    Initial commit

---
+++ hashTable.js
@@ -0,0 +1,84 @@
+export default function() {
+  const store = {};
+
+  function get(key) {
+    const hashValue = hash(key);
+    if (store[hashValue]) {
+      return store[hashValue][key];
+    } else {
+      return undefined;
+    }
+  }
+
+  function set(key, value) {
+    const hashValue = hash(key);
+    var pair = {};
+    pair[key] = value;
+    if (store[hashValue]){
+      store[hashValue][key] = value;
+    } else {
+      store[hashValue] = pair;
+    }
+  }
+
+  function size(){
+    let count = 0;
+    Object.keys(store).forEach(hashValue => count += Object.keys(store[hashValue]).length);
+    return count;
+  }
+  return { get, set, size };
+}
+
+function hash(key) {
+  return murmurhash2_32_gc(key, 3);
+}
+/**
+ * JS Implementation of MurmurHash2
+ *
+ * @author <a href="mailto:gary.court@gmail.com">Gary Court</a>
+ * @see http://github.com/garycourt/murmurhash-js
+ * @author <a href="mailto:aappleby@gmail.com">Austin Appleby</a>
+ * @see http://sites.google.com/site/murmurhash/
+ *
+ * @param {string} str ASCII only
+ * @param {number} seed Positive integer only
+ * @return {number} 32-bit positive integer hash
+ */
+
+function murmurhash2_32_gc(str, seed) {
+  var
+      l = str.length,
+      h = seed ^ l,
+      i = 0,
+      k;
+
+  while (l >= 4) {
+    k =
+        ((str.charCodeAt(i) & 0xff)) |
+        ((str.charCodeAt(++i) & 0xff) << 8) |
+        ((str.charCodeAt(++i) & 0xff) << 16) |
+        ((str.charCodeAt(++i) & 0xff) << 24);
+
+    k = (((k & 0xffff) * 0x5bd1e995) + ((((k >>> 16) * 0x5bd1e995) & 0xffff) << 16));
+    k ^= k >>> 24;
+    k = (((k & 0xffff) * 0x5bd1e995) + ((((k >>> 16) * 0x5bd1e995) & 0xffff) << 16));
+
+    h = (((h & 0xffff) * 0x5bd1e995) + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16)) ^ k;
+
+    l -= 4;
+    ++i;
+  }
+
+  switch (l) {
+    case 3: h ^= (str.charCodeAt(i + 2) & 0xff) << 16;
+    case 2: h ^= (str.charCodeAt(i + 1) & 0xff) << 8;
+    case 1: h ^= (str.charCodeAt(i) & 0xff);
+      h = (((h & 0xffff) * 0x5bd1e995) + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16));
+  }
+
+  h ^= h >>> 13;
+  h = (((h & 0xffff) * 0x5bd1e995) + ((((h >>> 16) * 0x5bd1e995) & 0xffff) << 16));
+  h ^= h >>> 15;
+
+  return h >>> 0;
+}
---
+++ hashTable.spec.js
@@ -0,0 +1,54 @@
+import HashTable from './hashTable';
+
+
+describe('HashTable', ()=> {
+
+  let hashTable;
+  
+  beforeEach(()=> {
+    hashTable = new HashTable();
+  });
+
+  it('returns undefined when a key is absent', ()=> {
+    expect(hashTable.get('monkey')).toEqual(undefined);
+  });
+
+  //hash tests are disable as we're making this private
+  xit('uses a hash function to hash a key', ()=> {
+    const expected = String('monkey'.length % 3);
+    expect(hashTable.hash('monkey')).toEqual(expected);
+  });
+
+
+  xit('gives a different hash value for a different key', ()=> {
+    const expected = String('elephant'.length % 3);
+    expect(hashTable.hash('elephant')).toEqual(expected);
+  });
+
+  it('returns the stored value when given a key', ()=> {
+    const expected = 'banana';
+    hashTable.set('monkey', 'banana');
+    expect(hashTable.get('monkey')).toEqual(expected);
+  });
+
+  it('handles hash collisions by allowing multiples keys under the same hash', ()=> {
+    hashTable.set('monkey', 'banana');
+    hashTable.set('gerbil', 'seeds');
+    expect(hashTable.get('monkey')).toEqual('banana');
+    expect(hashTable.get('gerbil')).toEqual('seeds');
+  });
+
+  it('overwrites the previous value when the same key is written to again', ()=> {
+    hashTable.set('monkey', 'banana');
+    hashTable.set('monkey', 'monkey chow');
+    expect(hashTable.get('monkey')).toEqual('monkey chow');
+  });
+
+  it('knows its size', ()=> {
+    hashTable.set('monkey', 'banana');
+    expect(hashTable.size()).toEqual(1);
+    hashTable.set('gerbil', 'seeds');
+    expect(hashTable.size()).toEqual(2);
+  });
+
+});
